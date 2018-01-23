3f837d425fafa54ade2409ff889b9c5e61d944de
ptflp <filippov.pa@s-vfu.ru>
2018-01-23 17:12:36 +0900

    installed mdm rbac

--- web/common/config/main.php
+++ web/common/config/main.php
@@ -5,9 +5,21 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ]
+    ],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+         'user' => [
+            'identityClass' => 'mdm\admin\models\User',
+            'loginUrl' => ['admin/user/login'],
+        ]
     ],
 ];
